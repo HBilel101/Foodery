73683c5918a150901f8fd4466ee1f9b74f1faff8
HBilel101 <hbilel992@gmail.com>
2022-06-24 15:21:26 +0100

    fixed tailwind css issue and the is fine now

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -4,7 +4,7 @@ import './Recipe.css'
 const Recipe =  (props)=>{
 return(
 <div className="flex justify-center my-5  text-center sm:mx-6" >
-  <a className="flex flex-col md:flex-row md:max-w-2xl rounded-lg mx-4 bg-white shadow-lg samesize card" href={props.youtube} target="_blank" rel="noreferrer">
+  <a className="flex flex-col md:flex-row md:max-w-2xl rounded-lg mx-4 bg-white shadow-lg samesize card" href={props.youtube} target="_blank" rel="noopener noreferrer">
     <img className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src= {props.src} alt="" />
     <div className="p-7 flex flex-col justify-start">
       <h4 className="text-gray-900 text-xl font-medium mb-2">{props.name}</h4>
