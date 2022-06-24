5cb2dddc7d90bbb9a51b87ab2a27acfd88fcac68
HBilel101 <hbilel992@gmail.com>
2022-06-24 14:18:51 +0100

    Updates

---
+++ precache-manifest.1ffb01fb8e6a302feb0be608f1784296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64be93be6099eaace2a9",
+    "url": "/Foodery/static/css/main.6c0457e5.chunk.css"
+  },
+  {
+    "revision": "64be93be6099eaace2a9",
+    "url": "/Foodery/static/js/main.64be93be.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/Foodery/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "f17007353c607cf2a020",
+    "url": "/Foodery/static/js/2.f1700735.chunk.js"
+  },
+  {
+    "revision": "28c150f8017559ec64ac",
+    "url": "/Foodery/static/js/runtime~main.28c150f8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Foodery/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "2b3a19b7aec9084f3280f20dbdbdabae",
+    "url": "/Foodery/index.html"
+  }
+];
