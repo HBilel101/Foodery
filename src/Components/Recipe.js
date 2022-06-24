8b310629209537a0deb5d7adcd518c2d1f120c34
HBilel101 <hbilel992@gmail.com>
2022-06-24 16:03:55 +0100

    deleted the content of Readme.md

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -3,7 +3,7 @@ import './Recipe.css'
 
 const Recipe =  (props)=>{
 return(
-<div className="flex justify-center my-5  text-center sm:mx-6" >
+<div className="flex justify-center my-5  text-center sm:mx-20" >
   <a className="flex flex-col md:flex-row md:max-w-2xl rounded-lg mx-4 bg-white shadow-lg samesize card" href={props.youtube} target="_blank" rel="noopener noreferrer">
     <img className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src= {props.src} alt="" />
     <div className="p-7 flex flex-col justify-start">
